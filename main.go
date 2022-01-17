6129b6a06542b3c1dc60038c7b0fece9383d4135
zhonglang <478125258@qq.com>
2022-01-17 21:58:05 +0800

    add header

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "<h1>Hello， this is goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprintf(w, "abouttttt")
+		fmt.Fprintf(w, "abouttttt<a href=\"https://www.baidu.com\">xxxxx</a>")
 	} else {
 		fmt.Fprintf(w, "<h1>11111NOT FOUND</h1>")
 	}
