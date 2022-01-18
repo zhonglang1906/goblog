8e4f41a59d9343cf57cb74e4cea61cb56bbadab0
zhonglang <478125258@qq.com>
2022-01-18 23:14:22 +0800

    0118

--- main.go
+++ main.go
@@ -5,19 +5,27 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "<h1>Hello， this is goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprintf(w, "abouttttt<a href=\"https://www.baidu.com\">xxxxx</a>")
+		fmt.Fprintf(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "<h1>11111NOT FOUND</h1>")
+		fmt.Fprintf(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	http.ListenAndServe(":3000", router)
 }
